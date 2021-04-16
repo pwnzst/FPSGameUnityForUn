64b72c1229665475bf62b633b14deede111379be
pwnzst <mermody747@gmail.com>
2021-04-16 06:23:24 +0200

    +Healthbar for enemies
    Healthbar for enemies was added

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public float enemyHealth = 100f;
+    public float enemyHealth;
+    public Slider slider;
+    public GameObject healthBarUI;
 
     public void DeductHealth(float deductHealth)
     {
         enemyHealth -= deductHealth;
+        slider.value -= deductHealth;
+        if (enemyHealth <= 0) {
+            EnemyDead();
+            slider.value = 0;
+           
+        }
+    }
 
-        if (enemyHealth <= 0) EnemyDead();
+  void Start()
+    {
+        slider.value = enemyHealth;
     }
 
 
